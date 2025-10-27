557d494b2599933018203c780050fcb4c7bdb565
afifah naufal rahmani <afifahrahmani466@gmail.com>
2025-10-27 14:09:19 +0700

    push

---
+++ Pertemuan 03/Praktikum/lib/material_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+// =======================
+// Stateless Widget
+// =======================
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            ListTile(title: Text('Home Page')),
+            ListTile(title: Text('About Page')),
+          ],
+        ),
+      ),
+      body: const Center(
+        child: BiggerText(teks: "Hello ULBI"), // Stateful Widget digunakan di sini
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
+      ]),
+    );
+  }
+}
+
+// =======================
+// Stateless Widget: Heading
+// =======================
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 28.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+// =======================
+// Stateful Widget: BiggerText
+// =======================
+class BiggerText extends StatefulWidget {
+  final String teks;
+
+  const BiggerText({Key? key, required this.teks}) : super(key: key);
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          widget.teks,
+          style: TextStyle(fontSize: _textSize),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          child: Text(_textSize == 16.0 ? "Perbesar" : "Perkecil"),
+          onPressed: () {
+            setState(() {
+              _textSize = _textSize == 16.0 ? 32.0 : 16.0;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
