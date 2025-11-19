90cfed2527f6feb30f4676bd38c061f2349d721b
afifah naufal rahmani <afifahrahmani466@gmail.com>
2025-11-19 15:35:01 +0700

    praktikum

---
+++ Pertemuan 07/Praktikum/p6_714230026/lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:p6_714230026/bottom_navbar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(useMaterial3: false),
+      home: DynamicBottomNavbar(),
+    );
+  }
+}
+
+class MyInput extends StatefulWidget {
+  const MyInput({super.key});
+
+  @override
+  State<MyInput> createState() => _MyInputState();
+}
+
+class _MyInputState extends State<MyInput> {
+  TextEditingController _controller = TextEditingController();
+  bool lightOn = false;
+  String? language;
+  bool agree = false;
+
+  void showSnackbar() {
+    if (language != null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Kamu memilih: $language'),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Input Widget')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'Write your name here...',
+                labelText: 'Your Name',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text('Hello, ${_controller.text}'),
+                    );
+                  },
+                );
+              },
+            ),
+
+            Switch(
+              value: lightOn,
+              onChanged: (bool value) {
+                setState(() {
+                  lightOn = value;
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(lightOn ? 'Light On' : 'Light Off'),
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+              },
+            ),
+
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                RadioListTile<String>(
+                  title: const Text('Dart'),
+                  value: 'Dart',
+                  groupValue: language,
+                  onChanged: (String? value) {
+                    setState(() {
+                      language = value;
+                      showSnackbar();
+                    });
+                  },
+                ),
+                RadioListTile<String>(
+                  title: const Text('Kotlin'),
+                  value: 'Kotlin',
+                  groupValue: language,
+                  onChanged: (String? value) {
+                    setState(() {
+                      language = value;
+                      showSnackbar();
+                    });
+                  },
+                ),
+                RadioListTile<String>(
+                  title: const Text('Swift'),
+                  value: 'Swift',
+                  groupValue: language,
+                  onChanged: (String? value) {
+                    setState(() {
+                      language = value;
+                      showSnackbar();
+                    });
+                  },
+                ),
+              ],
+            ),
+            CheckboxListTile(
+              title: const Text('Agree / Disagree'),
+              value: agree,
+              onChanged: (bool? value) {
+                setState(() {
+                  agree = value!;
+                });
+              },
+              controlAffinity: ListTileControlAffinity.leading,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
